d751e6108b382123011ab72a2d1129af4bc3f2db
AYeong-Jeon <ay98923@naver.com>
2024-08-16 10:05:41 +0900

    docs : update message

--- src/main/java/com/view/Main.java
+++ src/main/java/com/view/Main.java
@@ -6,11 +6,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
-
         MessageUtil message = new MessageUtil();
-        System.out.println(message.getHintMsg());
+        message.getHintMsg();
+        message.getBossMsg();
     }
 }
