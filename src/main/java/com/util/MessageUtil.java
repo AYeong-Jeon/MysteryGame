1c7a68b278b361ec53b7f10925b87873ca9157ab
AYeong-Jeon <ay98923@naver.com>
2024-08-16 19:19:34 +0900

    feat : update code

--- src/main/java/com/util/MessageUtil.java
+++ src/main/java/com/util/MessageUtil.java
@@ -3,7 +3,8 @@ package com.util;
 public class MessageUtil {
 
     public void getStartMsg() {
-
+        System.out.println("도시는 오랜 시간 동안 평온했지만, 최근 들어 미스터리한 사건들이 연달아 일어나며 주민들의 불안이 커지고 있습니다. " +
+                "명탐정성민은 도시에 도착하자마자 도시에 대한 첫 인상을 남길 수 있는 중대한 사건을 맞이하게 됩니다. ");
     }
 
     public void getHintMsg () {
