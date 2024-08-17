e22fde15fa346a9cc0892e6fa48f9bb12a27dcec
AYeong-Jeon <dkdud98923@gmail.com>
2024-08-17 16:20:20 +0900

    feat : add TimeUtil

--- src/main/java/com/util/TimeUtil.java
+++ src/main/java/com/util/TimeUtil.java
@@ -1,5 +1,28 @@
 package com.util;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
 public class TimeUtil {
-    //시작시간, 종료시간으로 총 플레이 시간 구하기 등
+    public String getNowTime() {
+        LocalTime now = LocalTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+        String formatedNow = now.format(formatter);
+        return formatedNow;
+    }
+
+    public String totalPlayTime(String start, String end) {
+        String[] startTimeArr = start.split(":");
+        String[] endTimeArr = end.split(":");
+
+        LocalTime startTime = LocalTime.of(Integer.parseInt(startTimeArr[0]), Integer.parseInt(startTimeArr[1]));
+        LocalTime endTime = LocalTime.of(Integer.parseInt(endTimeArr[0]), Integer.parseInt(endTimeArr[1]));
+        long hoursBetween = ChronoUnit.HOURS.between(startTime, endTime);
+        long minutesBetween = ChronoUnit.MINUTES.between(startTime, endTime) % 60;
+
+
+        return hoursBetween + "시간 " + minutesBetween + "분 ";
+    }
+
 }
